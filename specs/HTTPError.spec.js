323ab8e40d52d118182986fc9876b0dee54a6d36
jarvispact <jarvispact@gmail.com>
2018-07-18 21:01:30 +0200

    improved logging and add support to pass props in service.start()

--- specs/HTTPError.spec.js
+++ specs/HTTPError.spec.js
@@ -10,22 +10,37 @@ test('it should throw if argument "message" is missing', () => {
     expect(getError).toThrowError('missing argument: "message"');
 });
 
-test('it should throw if argument "code" is missing', () => {
-    const getError = () => HTTPError('badRequest', 'a message');
-    expect(getError).toThrowError('missing argument: "code"');
-});
-
 test('it should throw if argument "type" is invalid', () => {
     const getError = () => HTTPError('invalidType', 'a message', 'E_CODE');
     expect(getError).toThrowError('http error type: "invalidType" invalid');
 });
 
 test('it should return a http error object', () => {
+    const error = HTTPError('badRequest', 'a message');
+    expect(error.output.payload).toEqual({
+        statusCode: 400,
+        error: 'Bad Request',
+        message: 'a message',
+    });
+});
+
+test('it should return a http error object with a error code', () => {
     const error = HTTPError('badRequest', 'a message', 'E_CODE');
     expect(error.output.payload).toEqual({
         statusCode: 400,
         error: 'Bad Request',
         message: 'a message',
         code: 'E_CODE',
+    });
+});
+
+test('it should return a http error object with additional data', () => {
+    const error = HTTPError('badRequest', 'a message', 'E_CODE', { foo: 'bar' });
+    expect(error.output.payload).toEqual({
+        statusCode: 400,
+        error: 'Bad Request',
+        message: 'a message',
+        code: 'E_CODE',
+        data: { foo: 'bar' },
     });
 });
